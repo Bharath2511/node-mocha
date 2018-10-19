86ad478d592d93e0949518ff5f45d01663d7192b
V S Bharath <bharathcse5t6@gmail.com>
2018-10-19 12:16:42 +0530

    toBeA

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,18 +1,19 @@
+const expect = require('expect')
 const utils = require('./utils')
+
 
 
 it('should add two numbers',()=> {
   //code that tests add function
   //import util.js
-  var res = utils.add(33,11)  
-  if(res != 44) { 
-      throw new Error(`expected 44,but got ${res}`)
-  }
+  var res = utils.add(33,11) 
+  expect(res).toBe(44).toBeA('number')
+//   if(res != 44) { 
+//       throw new Error(`expected 44,but got ${res}`)
+//   }
 })
 
 it('should return square of a number',()=> {
     var res = utils.square(6)
-    if(res != 36) {
-        throw new Error(`expected 36,but got ${res }`)
-    }
+    expect(res).toBe(36).toBeA('number')
 })
