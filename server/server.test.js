ef4e894c678874eae9cc5d18230a560ca6398d15
V S Bharath <bharathcse5t6@gmail.com>
2018-10-19 18:23:42 +0530

    adding own tests

--- server/server.test.js
+++ server/server.test.js
@@ -1,34 +1,42 @@
 const request = require('supertest')
 const expect = require('expect')
 
-describe('Utils',()=>{
-    
-})
 
 var app = require('./server.js').app
 
-it('should return hello world',(done)=>{
-    request(app)
-     .get('/')
-     .expect(404)
-     .expect((res)=>{
-       expect(res.body).toInclude({
-           error :"page not found"
-       })
-     })
-     .end(done)
-})
+describe('server',()=>{
+    describe('/',()=>{
+        it('should return hello world',(done)=>{
+            request(app)
+             .get('/')
+             .expect(404)
+             .expect((res)=>{
+               expect(res.body).toInclude({
+                   error :"page not found"
+               })
+             })
+             .end(done)
+        })
+        
 
-it('should return status code of 200',(done)=>{
-    //requesting express application
-    request(app)
-     .get('/users')
-     .expect(200)
-     //res comes from supertest we can pass in headers,body
-     .expect((res)=>{
-         expect(res.body).toInclude({
-             name : 'bob',age : 5
+    })
+
+  describe('/users',()=>{
+    it('should return status code of 200',(done)=>{
+        //requesting express application
+        request(app)
+         .get('/users')
+         .expect(200)
+         //res comes from supertest we can pass in headers,body
+         .expect((res)=>{
+             expect(res.body).toInclude({
+                 name : 'bob',age : 5
+             })
          })
-     })
-     .end(done)
-})
+         .end(done)
+    })
+  })
+ })
+ 
+
+
