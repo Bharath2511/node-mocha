2c5ceda6b3012ebb3a52e8e38a51b8fe89924372
V S Bharath <bharathcse5t6@gmail.com>
2018-10-19 20:13:47 +0530

    rewiring

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,14 +1,19 @@
 const expect = require('expect')
+//rewire is used for swap out variables
 const rewire = require('rewire')
 
 var app = rewire('./app')
+//rewire adds two methods to our app they are
+//__set__ and __get__
 
 
 describe('App',()=>{
 
     var db = {
+        //creating a spy for db
         saveUser:expect.createSpy()
     }
+    //1)thing we want to replace and name wit hwhich we want to replace
     app.__set__('db',db)
 
     it('should call the spy correctly',()=>{
@@ -24,7 +29,7 @@ describe('App',()=>{
     it('should call saveuser with user object',()=>{
         var email = 'bob@gmail.com'
         var password = '123qa'
-
+        //function we want to test
         app.handleSignup(email,password)
         expect(db.saveUser).toHaveBeenCalledWith({email,password})
     })
